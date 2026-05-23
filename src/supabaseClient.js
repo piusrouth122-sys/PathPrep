62b7c2b34c6d95a425f070b0f827e94c02fcc237
piusrouth122-sys <piusrouth122@gmail.com>
2026-05-23 19:57:36 +0530

    Fix database connection variables

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,5 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
+// Vite requires 'import.meta.env' instead of the standard 'process.env'
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
